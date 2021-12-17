bf171d8e1aa10c0414561d710c1271d92a2358c9
rseth <rseth@pdx.edu>
2021-12-17 15:32:17 -0800

    looks like its the rep that's signed actually

--- cmd_shell/file_transfer.py
+++ cmd_shell/file_transfer.py
@@ -104,7 +104,7 @@ def file_upload(filepath: str,
                 fails += 1
                 continue
 
-            if reply[0] != (zlib.crc32(seg) % 2**32):
+            if (reply[0]% 2**32) != (zlib.crc32(seg) % 2**32):
                 print('Got CRC32 = ', reply[0] , '|| Expected CRC32 = ', (zlib.crc32(seg) %2**32))
                 fails += 1
             else:
