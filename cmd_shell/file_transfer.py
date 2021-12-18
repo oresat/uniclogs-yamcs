3fc4a646a2441fb5af9a320f4225bcab439209a1
rseth <rseth@pdx.edu>
2021-12-17 18:18:16 -0800

    removed bad assumption about reply - reply is actually incorrect, reporting that correctly

--- cmd_shell/file_transfer.py
+++ cmd_shell/file_transfer.py
@@ -104,8 +104,8 @@ def file_upload(filepath: str,
                 fails += 1
                 continue
 
-            if (reply[0] + 2**32) != (zlib.crc32(seg) % 2**32):
-                print('Got CRC32 = ', (reply[0] + 2**32) , '|| Expected CRC32 = ', (zlib.crc32(seg) %2**32))
+            if (reply[0]) != (zlib.crc32(seg) % 2**32):
+                print('Got CRC32 = ', (reply[0]) , '|| Expected CRC32 = ', (zlib.crc32(seg) %2**32))
                 fails += 1
             else:
                 print('  seg', i, 'ack')
