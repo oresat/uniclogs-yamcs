21f6e5629287a10b547f637560deeb6c88293cfd
rseth <rseth@pdx.edu>
2022-01-11 16:46:17 -0800

    correcting crc response to unsigned...

--- cmd_shell/file_transfer.py
+++ cmd_shell/file_transfer.py
@@ -104,10 +104,11 @@ def file_upload(filepath: str,
                 fails += 1
                 continue
 
-            if (reply[0]) != (zlib.crc32(seg) % 2**32):
-                print('Got CRC32 = ', (reply[0]) , '|| Expected CRC32 = ', (zlib.crc32(seg) %2**32))
+            if (reply[0] + 2**32) != (zlib.crc32(seg) % 2**32):
+                print('Got CRC32 = ', (reply[0] + 2**32) , '|| Expected CRC32 = ', (zlib.crc32(seg) %2**32))
                 fails += 1
             else:
+                print("got correct CRC32 = ", (reply[0] + 2**32))
                 print('  seg', i, 'ack')
                 break  # segment sent was successful
 
